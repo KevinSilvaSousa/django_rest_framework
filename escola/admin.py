f768bf5fbbc9129856f2e452e6a7efdb4c331257
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-04-14 15:27:06 -0300

    Commit Inicial

---
+++ escola/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from escola.models import Estudante, Curso
+
+class Estudantes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_displau_links = ('id', 'nome')
+    list_per_page = 20
+    search_fields = ('nome', )
+
+admin.site.register(Estudante, Estudantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_displau_links = ('id', 'codigo', )
+    search_fields = ('codigo', )
+
+
+admin.site.register(Curso, Cursos)
+
