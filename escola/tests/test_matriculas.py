b530f31c53ae56401962691a1df82c142d93510b
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-04-23 15:47:38 -0300

    Commit

--- escola/tests/test_matriculas.py
+++ escola/tests/test_matriculas.py
@@ -2,50 +2,44 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from escola.models import Estudante, Curso, Matricula
 
 class MatriculasTestCase(APITestCase):
+    fixtures = ['prototipo_banco.json']
     def setUp(self):
-        self.usuario = User.objects.create_superuser(username='admin', password='admin')
+        self.usuario = User.objects.get(username='kevin')
         self.url = reverse('Matriculas-list')
         self.client.force_authenticate(user=self.usuario)
-        self.estudante_01 = Estudante.objects.create(
-            nome = 'Teste estudante UM',
-            email = 'testeestudante01@gmail.com',
-            cpf = '20452543053',
-            data_nascimento = '2024-01-01',
-            celular = '86 99999-9999'
-        )
-        self.estudante_02 = Estudante.objects.create(
-            nome = 'Teste estudante DOIS',
-            email = 'testeestudante02@gmail.com',
-            cpf = '72803240025',
-            data_nascimento = '2024-01-02',
-            celular = '86 99999-9999'
-        )
-    def test_requisicao_get_para_listar_estudantes(self):
-        """ Teste de requisicao GET"""
-        response = self.client.get(self.url) #/estudantes/
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.estudante = Estudante.objects.get(pk=1)
+        self.curso = Curso.objects.get(pk=1)
+        self.matricula = Matricula.objects.get(pk=1)
     
-    
-    def test_requisicao_get_para_listar_um_estudante(self):
-        """ Teste de requisicao GET para um estudante"""
-        response = self.client.get(self.url+'1/') #/estudantes/1/
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        dados_estudante = Estudante.objects.get(pk=1)
-        dados_estudante_serializados = EstudanteSerializer(instance=dados_estudante).data
-        self.assertEqual(response.data, dados_estudante_serializados)
+    def test_requisicao_get_para_listar_matriculas(self):
+        """Teste para verificar a requisição GET para listar as matriculas"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
 
-
-
-    def test_requisicao_post_para_criar_um_estudante(self):
-        """ Teste de requisicao POST para um estudante"""
+    def test_requisicao_post_para_criar_matricula(self):
+        """Teste para verificar a requisição POST para criar uma matrícula"""
         dados = {
-            'nome': 'teste',
-            'email': 'teste@gmail.com',
-            'cpf': '11928684041',
-            'data_nascimento': '2003-05-04',
-            'celular': '11 99999-9999'
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'M'
         }
         response = self.client.post(self.url, data=dados)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_para_nao_deletar_matricula(self):
+        """Teste para verificar a requisição DELETE não autorizada para deletar uma matricula"""
+        response = self.client.delete(f'{self.url}1/')
+        self.assertEqual(response.status_code,status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_requisicao_put_para_atualizar_matricula(self):
+        """Teste para verificar a requisição PUT não permitida para atualizar uma matricula"""
+        dados = {
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'V'
+        }
+        response = self.client.put(f'{self.url}1/', data=dados)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
